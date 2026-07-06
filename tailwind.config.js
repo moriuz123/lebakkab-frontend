62a3536a586485ee96b2822740e64e9d2342af2f
moriuz123 <putra.lebakk@gmail.com>
2026-07-06 12:45:01 +0700

    fix: disable broken dark mode causing invisible text on detail pages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class', // Disable automatic dark mode based on prefers-color-scheme
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
