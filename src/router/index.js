42ea126e1da646d63b11e147471fcf4ac6bcf027
moriuz123 <putra.lebakk@gmail.com>
2026-07-16 08:39:57 +0700

    feat: implement frontend for PPID Module

--- src/router/index.js
+++ src/router/index.js
@@ -29,6 +29,7 @@ import PejabatPage from '@/views/PejabatPage.vue'
 import PejabatDetail from '@/views/PejabatDetail.vue'
 import ProfilKabupaten from '@/views/ProfilKabupaten.vue'
 import SponTtePage from '@/views/SponTtePage.vue' // ✅ import SPON TTE
+import PpidPage from '@/views/PpidPage.vue' // ✅ import PPID
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -196,6 +197,12 @@ const router = createRouter({
       name: 'SearchPage',
       component: SearchPage,
       props: (route) => ({ q: route.query.q }),
+    },
+    {
+      path: '/ppid',
+      name: 'ppid',
+      component: PpidPage,
+      meta: { title: 'Layanan PPID - Kab. Lebak' }
     },
     {
       path: '/spon-tte',
